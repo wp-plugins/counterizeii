6308a99b3b15085a79000a637f4734c4cecf075c
forki <forki@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2007-12-17 08:08:41 +0000

    git-svn-id: https://plugins.svn.wordpress.org/counterizeii/tags/2.12.3.0@26905 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- counterize.php
+++ counterize.php
@@ -3,12 +3,14 @@
  Plugin Name: Counterize II
  Plugin URI: http://www.navision-blog.de/counterize
  Description: Simple counter-plugin with no external libs - saves IP, timestamp, visited URl, referring URl and browserinformation in database, and can display total hits, unique hits and other statistics in WordPress webpages. Admin-interface available with detailed information...
- Version: 2.12.2
+ Version: 2.12.3
  Author: Steffen Forkmann
  Author URI: http://navision-blog.de
 */
 
 /*
+ New in 2.12.3
+ - fixed installtion
  New in 2.12.1 /n 2.12.2
  - fixed folder
  New in 2.12
