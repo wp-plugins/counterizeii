a97494670ac912141d45af8e5678c0714526bee7
forki <forki@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-04-08 10:16:56 +0000

    Counterize II Übersetzung - Johannes Parb
    git-svn-id: https://plugins.svn.wordpress.org/counterizeii/trunk@39182 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- counterize_dashboard.php
+++ counterize_dashboard.php
@@ -15,9 +15,9 @@ function counterize_dashboard()
 		  <div class='dashboard-widget' style='width: 300px; height: 150px;'>
   			<h3 class='dashboard-widget-title'><?php _e('Counterize II Status'); ?> <a href='edit.php?page=counterizeii/counterize.php'>&raquo;</a></h3>
   			<div class='dashboard-widget-content' >
-  		  	<p><?php _e('Total: '); ?> <strong><?php echo $count; ?></strong> <?php _e('hits and ')?> <strong><?php echo $unique; ?></strong> <?php _e(' unique.'); ?><br />
-	        <?php _e('Today: '); ?><strong><?php echo $todaycount; ?></strong> <?php _e('hits and '); ?><strong><?php echo $todayunique; ?></strong><?php _e(' unique.'); ?><br />
-	        <?php _e('Currently: '); ?><strong><?php echo $online; ?></strong><?php _e(' users online.'); ?></p>
+  		  	<p><?php _e('Total: ','counterize'); ?> <strong><?php echo $count; ?></strong> <?php _e('hits and ','counterize')?> <strong><?php echo $unique; ?></strong> <?php _e(' unique.'); ?><br />
+	        <?php _e('Today: ','counterize'); ?><strong><?php echo $todaycount; ?></strong> <?php _e('hits and ','counterize'); ?><strong><?php echo $todayunique; ?></strong><?php _e(' unique.'); ?><br />
+	        <?php _e('Currently: ','counterize'); ?><strong><?php echo $online; ?></strong><?php _e(' users online.','counterize'); ?></p>
        </div>
      </div>
   <?php
